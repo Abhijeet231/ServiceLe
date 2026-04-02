a786b84912b7f3c8225eb291a6d1eb08e87e4527
Abhijeet <pikupc01@gmail.com>
2026-04-02 13:12:34 +0530

    feat: created different required modules folders defined User Schema and added typesaftey by using an interface

---
+++ backend/src/modules/user/user.schema.ts
@@ -0,0 +1,105 @@
+import mongoose, { Document, Model } from "mongoose";
+
+// Defining Interface for User 
+export interface IUser extends Document {
+    name: string;
+    email: string;
+    password: string;
+    role: "customer" | "provider" | "admin";
+    refreshToken?: string;
+    avatar?: {
+        url?: string;
+        public_id?: string;
+    };
+    isVerified: boolean;
+    resetPasswordToken?: string;
+    resetPasswordExpires?: Date;
+    address: string;
+    location: {
+        type: "Point";
+        coordinates: [number, number]; // [lng, lat]
+    };
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+
+// Defining DB Schema
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required!"],
+        trim: true,
+        minlength: [2, "Name must be at least 2 characters long!"],
+        maxlength: [50, "Name must be less than 50 characters long!"]
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required!"],
+        unique: true,
+        trim: true,
+        match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "please use a valid email address"],
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required!"],
+        minlength: [6, "Password must be at least 6 characters long!"],
+        select: false
+    },
+    role: {
+        type: String,
+        enum: ["customer", "provider", "admin"],
+        default: "customer"
+    },
+    refreshToken: {
+        type: String,
+        select: false
+    },
+    avatar: {
+        url: { type: String },
+        public_id: { type: String }
+
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    resetPasswordToken: {
+        type: String,
+        select: false
+    },
+    resetPasswordExpires: {
+        type: Date,
+        select: false
+    },
+
+    // Address fileds
+    address: {
+        type: String,
+        required: true
+    },
+
+    // GeoJSON location format
+    location: {
+        type: {
+            type: String,
+            enum: ["Point"],
+            default: "Point",
+        },
+        coordinates: {
+            type: [Number],
+            required: true,
+        }
+    }
+
+}, { timestamps: true })
+
+
+// Location  indexing
+userSchema.index({ location: "2dsphere" });
+
+// Typed Model
+const User: Model<IUser> = mongoose.model<IUser>("User", userSchema);
+
+export default User;
